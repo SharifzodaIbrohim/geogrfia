f247e04043d7a423f1e78ea649d2e4f47fa4fe8e
Sharifzoda Ibrohim <ibrohimicamsed316@gmail.com>
2026-08-23 12:57:09 +0500

    RESTORE students-reg: concat loader for 2 plain halves (1-page print)

--- js/admin-students-reg.js
+++ js/admin-students-reg.js
@@ -1,18 +1,9 @@
 (function(){
-  var files=["/js/_asr_gz_0.txt","/js/_asr_gz_1.txt","/js/_asr_gz_2.txt"];
-  Promise.all(files.map(function(f){
+  Promise.all(["/js/_asr_half_a.txt","/js/_asr_half_b.txt"].map(function(f){
     return fetch(f,{credentials:"same-origin"}).then(function(r){
       if(!r.ok) throw new Error(f+" "+r.status);
       return r.text();
     });
-  })).then(function(parts){
-    var b64=parts.join("");
-    var bin=atob(b64);
-    var bytes=new Uint8Array(bin.length);
-    for(var i=0;i<bin.length;i++) bytes[i]=bin.charCodeAt(i);
-    var ds=new DecompressionStream("gzip");
-    var stream=new Blob([bytes]).stream().pipeThrough(ds);
-    return new Response(stream).text();
-  }).then(function(text){ (0,eval)(text); })
+  })).then(function(p){ (0,eval)(p.join("")); })
   .catch(function(e){ console.error("[students-reg]",e); });
 })();

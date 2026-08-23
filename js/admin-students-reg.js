11a72bef4576d5b24477d03e8b6f9a93baad4a7f
Sharifzoda Ibrohim <ibrohimicamsed316@gmail.com>
2026-08-23 12:28:37 +0500

    students-reg: loader for full Даватнома payload (1-page print + IG QR)

--- js/admin-students-reg.js
+++ js/admin-students-reg.js
@@ -1,1 +1,19 @@
-PLACEHOLDER
+/* admin-students-reg loader */
+(function () {
+  var files = ["js/_asr_b64_00.txt", "js/_asr_b64_01.txt", "js/_asr_b64_02.txt"];
+  Promise.all(files.map(function (f) {
+    return fetch(f, { credentials: "same-origin" }).then(function (r) {
+      if (!r.ok) throw new Error(f + " " + r.status);
+      return r.text();
+    });
+  })).then(function (parts) {
+    var b64 = parts.join("").replace(/\s+/g, "");
+    var bin = atob(b64);
+    var bytes = new Uint8Array(bin.length);
+    for (var i = 0; i < bin.length; i++) bytes[i] = bin.charCodeAt(i);
+    var code = new TextDecoder("utf-8").decode(bytes);
+    (0, eval)(code);
+  }).catch(function (e) {
+    console.error("[students-reg loader]", e);
+  });
+})();

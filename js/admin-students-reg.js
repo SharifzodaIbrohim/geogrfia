64f9e70d3c26a3bad4f7ec2507e24aa1a805d8c3
Sharifzoda Ibrohim <ibrohimicamsed316@gmail.com>
2026-08-23 12:59:17 +0500

    students-reg loader: 3 base64 chunks (full reg + 1-page print)

--- js/admin-students-reg.js
+++ js/admin-students-reg.js
@@ -1,9 +1,14 @@
 (function(){
-  Promise.all(["/js/_asr_half_a.txt","/js/_asr_half_b.txt"].map(function(f){
+  var F=["/js/_asr_b64_0.txt","/js/_asr_b64_1.txt","/js/_asr_b64_2.txt"];
+  Promise.all(F.map(function(f){
     return fetch(f,{credentials:"same-origin"}).then(function(r){
       if(!r.ok) throw new Error(f+" "+r.status);
       return r.text();
     });
-  })).then(function(p){ (0,eval)(p.join("")); })
-  .catch(function(e){ console.error("[students-reg]",e); });
+  })).then(function(parts){
+    var bin=atob(parts.join("").replace(/\s+/g,""));
+    var bytes=new Uint8Array(bin.length);
+    for(var i=0;i<bin.length;i++) bytes[i]=bin.charCodeAt(i);
+    (0,eval)(new TextDecoder("utf-8").decode(bytes));
+  }).catch(function(e){ console.error("[students-reg]",e); });
 })();

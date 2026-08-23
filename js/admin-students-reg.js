7662d68b67a53a5e12ebca4df9911b6d1f2eb94f
Sharifzoda Ibrohim <ibrohimicamsed316@gmail.com>
2026-08-23 13:09:57 +0500

    students-reg loader: 4 plain parts (_asr_p0..p3)

--- js/admin-students-reg.js
+++ js/admin-students-reg.js
@@ -1,5 +1,6 @@
 (function(){
-  Promise.all(["/js/_asr_half_a.txt","/js/_asr_half_b.txt"].map(function(f){
+  var F=["/js/_asr_p0.txt","/js/_asr_p1.txt","/js/_asr_p2.txt","/js/_asr_p3.txt"];
+  Promise.all(F.map(function(f){
     return fetch(f,{credentials:"same-origin"}).then(function(r){
       if(!r.ok) throw new Error(f+" "+r.status);
       return r.text();

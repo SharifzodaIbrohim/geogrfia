3155f170b26a9e16b11092179539f304c270afc7
Sharifzoda Ibrohim <ibrohimicamsed316@gmail.com>
2026-08-23 12:50:57 +0500

    students-reg: gzip loader for compact 1-page + black IG QR

--- js/admin-students-reg.js
+++ js/admin-students-reg.js
@@ -1,10 +1,18 @@
 (function(){
-  var files=["/js/_asr_b64_00.txt","/js/_asr_b64_01.txt","/js/_asr_b64_02.txt","/js/_asr_b64_03.txt"];
+  var files=["/js/_asr_gz_0.txt","/js/_asr_gz_1.txt","/js/_asr_gz_2.txt"];
   Promise.all(files.map(function(f){
     return fetch(f,{credentials:"same-origin"}).then(function(r){
       if(!r.ok) throw new Error(f+" "+r.status);
       return r.text();
     });
-  })).then(function(p){ (0,eval)(p.join("")); })
+  })).then(function(parts){
+    var b64=parts.join("");
+    var bin=atob(b64);
+    var bytes=new Uint8Array(bin.length);
+    for(var i=0;i<bin.length;i++) bytes[i]=bin.charCodeAt(i);
+    var ds=new DecompressionStream("gzip");
+    var stream=new Blob([bytes]).stream().pipeThrough(ds);
+    return new Response(stream).text();
+  }).then(function(text){ (0,eval)(text); })
   .catch(function(e){ console.error("[students-reg]",e); });
 })();

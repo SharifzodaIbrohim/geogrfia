0087cbdebdcef4b6d0e84eeb19929edc9395fbf9
Sharifzoda Ibrohim <ibrohimicamsed316@gmail.com>
2026-08-31 03:04:27 +0500

    students-reg loader: continuous root _asr_x b64 (atob-safe)

--- js/admin-students-reg.js
+++ js/admin-students-reg.js
@@ -1,12 +1,17 @@
 (function(){
   var F=[];
-  for (var i=0;i<8;i++) F.push("/js/_asr8_"+i+".js");
+  for (var i=0;i<24;i++) F.push("/_asr_x"+i+".txt");
   Promise.all(F.map(function(f){
     return fetch(f,{credentials:"same-origin"}).then(function(r){
       if(!r.ok) throw new Error(f+" "+r.status);
       return r.text();
     });
   })).then(function(parts){
-    (0,eval)(parts.join(""));
+    var b64 = parts.join("").replace(/\s+/g,"");
+    var bin = atob(b64);
+    var out = "";
+    for (var i=0;i<bin.length;i++) out += String.fromCharCode(bin.charCodeAt(i));
+    try { out = decodeURIComponent(escape(out)); } catch(_) {}
+    (0,eval)(out);
   }).catch(function(e){ console.error("[students-reg] load failed", e); });
 })();
